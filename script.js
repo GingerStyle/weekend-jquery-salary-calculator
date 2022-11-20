19bb9b0b619b973df3dd0f2f82e52c56fb8ca78c
Myles Voigt <myles.voigt@gmail.com>
2022-11-20 15:48:40 -0600

    setup html file

---
+++ script.js
@@ -0,0 +1,12 @@
+$(onReady);
+
+function onReady(){
+    console.log('jquery is working');
+    $('#first-name-input').val('First Name');
+    $('#last-name-input').val('Last Name');
+    $('#id-input').val('ID');
+    $('#title-input').val('Title');
+    $('#annual-salary-input').val('Annual Salary');
+
+    
+}
